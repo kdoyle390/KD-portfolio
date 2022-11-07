ce375f9a0c551f578976595c0bdd7276b295e8b9
kdoyle390 <kdoyle390@gmail.com>
2022-11-07 12:48:17 -0600

    Cleaned up a little code, stacked card text and logo over border image. Need to resize logos. Moon and Chalice are a little too large.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { faClipboardList, faHourglassStart } from '@fortawesome/free-solid-svg-i
 import { faHourglassHalf } from '@fortawesome/free-solid-svg-icons';
 import { faHourglassEnd } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import tarotCup from './imgs/tarotCup.png';
+import tarotMoon from './imgs/tarotMoon.png';
+import tarotSun from './imgs/tarotSun.png';
+import tarotOutline from './imgs/tarotOutline.png';
 
-// need to figure out how to rotate cards
 
 function App() {
 
   
   const timeTitle = ['Past', 'Present', 'Future']
-  const iconList = [];
+  const iconList = [tarotOutline, tarotMoon, tarotSun, tarotCup];
 
-  iconList.push(<FontAwesomeIcon icon={faHourglassStart} />);
-  iconList.push(<FontAwesomeIcon icon={faHourglassHalf} />);
-  iconList.push(<FontAwesomeIcon icon={faHourglassEnd} />)
+  // iconList.push();
+  // iconList.push(<FontAwesomeIcon icon={faHourglassHalf} />);
+  // iconList.push(<FontAwesomeIcon icon={faHourglassEnd} />)
 
   return (
     <div className="App">
@@ -27,13 +30,13 @@ function App() {
       <div className="container text-center">
         <div className="row">
           <div className="col-lg-4 col-md-12 card-container">
-            <Card timeTitle={timeTitle[0]} iconList={iconList[0]} id='card-left' /> 
+            <Card timeTitle={timeTitle[0]} border={iconList[0]} iconList={iconList[1]} id='card-left' /> 
           </div>
           <div className="col-lg-4 col-md-12 card-container">
-            <Card timeTitle={timeTitle[1]} iconList={iconList[1]} id='card-center' /> 
+            <Card timeTitle={timeTitle[1]} border={iconList[0]} iconList={iconList[2]} id='card-center sun' /> 
           </div>
           <div className="col-lg-4 col-md-12 card-container">
-            <Card timeTitle={timeTitle[2]} iconList={iconList[2]} id='card-right'/> 
+            <Card timeTitle={timeTitle[2]} border={iconList[0]} iconList={iconList[3]} id='card-right'/> 
           </div>
         </div>
       </div>
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import tarotCup from './imgs/tarotCup.png';
+import tarotMoon from './imgs/tarotMoon.png';
+import tarotSun from './imgs/tarotSun.png';
+import tarotOutline from './imgs/tarotOutline.png';
 
-function Card({timeTitle, iconList}) {
+
+function Card({timeTitle, border, iconList}) {
  
     return(
         <>
@@ -8,13 +13,12 @@ function Card({timeTitle, iconList}) {
         <div className="flip-card">
             <div className="flip-card-inner">
                 <div className="flip-card-front">
-                    <p className="fa-icon-styling">
-                        {iconList}
-                    </p>
-                    <img src="./imgs/tarotCup.png" />
+                    <img className="tarot-border" src={border} />
+                    <img className="tarot-logo" src={iconList} />
                     <h1 className="card-title">{timeTitle}</h1>
                 </div>
                 <div className="flip-card-back">
+                    <img className="tarot-border" src={border} />
                     <p>back of card content</p>
                 </div>
             </div>
