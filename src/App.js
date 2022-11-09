c7fc3387e8b3376209a2101c8f6383bb2cfae4eb
kdoyle390 <kdoyle390@gmail.com>
2022-11-09 16:26:59 -0600

    Created buttons and coded download and link functionality. Updated card backs and fixed styling. Will need to continue to update styling.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import tarotCup from './imgs/tarotCup.png';
 import tarotMoon from './imgs/tarotMoon.png';
 import tarotSun from './imgs/tarotSun.png';
 import tarotOutline from './imgs/tarotOutline.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { envelope } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faDownload  } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function App() {
 
 
-  const emailIcon = <FontAwesomeIcon icon="fa-solid fa-envelope" />;
+  // const emailIcon = <FontAwesomeIcon icon={faEnvelope} />
+  const resumeIcon = <FontAwesomeIcon icon={faDownload} />
+  const onButtonClick = () => {fetch('Kelsey Doyle_Resume.pdf')
+        .then(response => {response.blob()
+        .then(blob => {const fileUrl = window.URL.createObjectURL(blob);
+            let alink = document.createElement('a');
+            alink.href = fileUrl; 
+            alink.download = 'Kelsey Doyle_Resume.pdf'; 
+            alink.click(); 
+          }) 
+        }) 
+      }
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+
   const timeTitle = ['Past', 'Present', 'Future']
   const iconList = [tarotOutline, tarotMoon, tarotSun, tarotCup];
-  const backText = ["Over 7 years of experience in the test-development industry creating high-stakes examinations for physicians exiting their residency programs. Duties included careful analysis of item-bank contents, creation of item-writing assignments to cover areas of need, and coordinating production of meeting materials for over 30 yearly meetings.", 
-      "After obtaining a Certificate of Completion from Flatiron School's Software Engineering program...", 
-      "Working in the non-profit sector using my software engineering skills to find solutions to complex problems related to housing, food insecurity, public services, etc."]
+  const backText = ["Over 7 years of experience in the test-development industry creating high-stakes examinations for physicians.", 
+      "Software Engineering Certificate of Completion holder from Flatiron School. Searching for a Fullstack role.", 
+      "Working in the non-profit sector using SE skills to find solutions to complex problems related to housing, food insecurity, or other public services."]
 
   return (
     <div className="App">
@@ -24,6 +41,24 @@ function App() {
         <p id="bio-text">
             Hi there! Welcome to my page. Hover over the tarot cards below to learn a little bit more about me and my software engineering journey.
         </p>
+        <div className="button-container container">
+          <div className="row">
+            <button onClick={onButtonClick} className="btn btn-outline-dark button-link col">
+              {resumeIcon} Resume
+            </button> 
+            {/* <button className="btn btn-outline-dark button-link col"> 
+              {emailIcon} Email
+            </button > */}
+            <button onClick={() => openInNewTab('https://www.linkedin.com/in/kelsey-doyle-chi/')} target="_blank" className="btn btn-outline-dark button-link col">
+              LinkedIn
+            </button >
+            <button onClick={() => openInNewTab('https://github.com/kdoyle390')} className="btn btn-outline-dark button-link col">
+             Github
+            </button>
+           
+
+          </div>
+        </div>
       </div>
 
       <div className="container text-center">
